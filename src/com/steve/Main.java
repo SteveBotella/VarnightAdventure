126d99e2686b87525e142897806f1d49ad48f100
Steve Botella <steve.botella@le-campus-numerique.fr>
2021-04-28 15:07:01 +0200

    feat: Project Hierarchy

--- src/com/steve/Main.java
+++ src/com/steve/Main.java
@@ -1,8 +1,15 @@
 package com.steve;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner sc = new Scanner(System.in);
+        int characterLvl = 0;
+
+        System.out.println("--- Create Character ---");
+        characterLvl = sc.nextInt();
+        System.out.println(characterLvl);
     }
 }
