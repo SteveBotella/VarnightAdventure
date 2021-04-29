8eaec960ab476406a543e58a7eeca1217681fd73
Steve <stevebotella@hotmail.fr>
2021-04-29 10:07:28 +0200

    wip: Create the Mage selection

--- src/ui/Menu.java
+++ src/ui/Menu.java
@@ -1,5 +1,6 @@
 package ui;
 
+import actors.Mage;
 import actors.Warrior;
 import com.steve.Main;
 
@@ -72,12 +73,58 @@ public class Menu {
                     System.out.println("     We call him :                                                              ");
                     System.out.println("               --- " + warrior.getName() + " !!! ---                            ");
                 }
-                System.out.println("     (Now you can use the 3 Numpad input to display your Character Sheet         ");
+                System.out.println("     (Now you can use the 9 Numpad input to display your Character Sheet         ");
                 System.out.println("     (Try it now : )                                                             ");
                 this.controllerChoiceInt = sc.nextInt();
-                if (controllerChoiceInt == 3) {
+                if (controllerChoiceInt == 9) {
                     warrior.characterSheet();
                 }
+            } else if (controllerChoiceInt == 2) {
+                Mage mage = new Mage();
+                System.out.println("     Choose a name that feel Heroic, with muscles & Raaaaaage !!!               ");
+                System.out.println("     (PS : No... DarkSasukeDu69 is not Skilled... But Varnight...)              ");
+                System.out.println("     (Any way, take the name you want... But Varnight...)                       ");
+                mage.setName(this.controllerChoiceString = sc.next());
+                if (controllerChoiceString != "") {
+                    System.out.println("     Once upon a time, in a Far far away Kingdom of peace & love,            ");
+                    System.out.println("     Lived a Strong, Proud & Honorable Family !                              ");
+                    System.out.println("             ----------                                                      ");
+                    System.out.println("            |   Next > |                                                     ");
+                    System.out.println("             ----------                                                      ");
+                    System.out.println("             0 (Numpad)                                                      ");
+                }
+                this.controllerChoiceInt = sc.nextInt();
+                if (controllerChoiceInt == 0) {
+                    System.out.println("     They worked hard on day and enjoy their life at night...                ");
+                    System.out.println("     So... One day comes a new born                                          ");
+                    System.out.println("             ----------                                                      ");
+                    System.out.println("            |   Next > |                                                     ");
+                    System.out.println("             ----------                                                      ");
+                    System.out.println("             0 (Numpad)                                                      ");
+                }
+                this.controllerChoiceInt = sc.nextInt();
+                if (controllerChoiceInt == 0) {
+                    System.out.println("     Likes his Dad he was Proud, likes her Mother he was Strong !            ");
+                    System.out.println("     Likes his family, he was Honorable...                                   ");
+                    System.out.println("             ----------                                                      ");
+                    System.out.println("            |   Next > |                                                     ");
+                    System.out.println("             ----------                                                      ");
+                    System.out.println("             0 (Numpad)                                                      ");
+                }
+                this.controllerChoiceInt = sc.nextInt();
+                if (controllerChoiceInt == 0) {
+                    System.out.println("     And here he comes ! Today is a great day to raid Dungeons & burn Villages !");
+                    System.out.println("     Here comes... The WARRIOR !!!                                              ");
+                    System.out.println("     We call him :                                                              ");
+                    System.out.println("               --- " + mage.getName() + " !!! ---                            ");
+                }
+                System.out.println("     (Now you can use the 9 Numpad input to display your Character Sheet         ");
+                System.out.println("     (Try it now : )                                                             ");
+                this.controllerChoiceInt = sc.nextInt();
+                if (controllerChoiceInt == 9) {
+                    mage.characterSheet();
+                }
+
             }
         } if (controllerChoiceInt == 2){
             System.out.println("                     Quit Game                     ");
